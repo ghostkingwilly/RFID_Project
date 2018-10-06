e185df44c9e8aa5e3388ac906f0c8961121fa0c0
Willy <willyhome85@gmail.com>
2018-10-06 14:35:37 +0800

    phase with no object moing

--- Code/Simulation/plot_traj.m
+++ Code/Simulation/plot_traj.m
@@ -1,6 +1,6 @@
 function [output] = plot_traj(X_ini, Y_ini, obj_mov, hand_mov, size, mod, fast)
     
-    subplot(2,1,1);
+    ax2 = subplot(2,1,1);
     end_x = max([max(obj_mov(1,:)) max(hand_mov(1,:))]);
     end_y = max([max(obj_mov(2,:)) max(hand_mov(2,:))]);
     ax_start = [min(X_ini) - 10, min(Y_ini) - 10];
@@ -9,7 +9,9 @@ function [output] = plot_traj(X_ini, Y_ini, obj_mov, hand_mov, size, mod, fast)
     %xlim([0 length(plot_size)]);
     %ylim([0 length(plot_size)]);
     
-    plot(X_ini, Y_ini, 'o');
+    plot(X_ini(2:3), Y_ini(2:3), 'o');
+    hold on;
+    plot(X_ini(1), Y_ini(1), 'x');
     hold on;
     
     h_hand = plot(hand_mov(1,:), hand_mov(2,:));
@@ -23,11 +25,15 @@ function [output] = plot_traj(X_ini, Y_ini, obj_mov, hand_mov, size, mod, fast)
     title('Trajectory Static');
     legend({'Start','Hand','Object'},'FontSize', 6,'Location','northwest')
     legend('boxoff');
+    xlabel(ax2,'Movement(cm.)', 'Color', [0 0.5 0]);
+    ylabel(ax2,'Movement(cm.)', 'Color', [0 0.5 0]);
     
     %figure;
-    subplot(2,1,2);
+    ax1 = subplot(2,1,2);
     grid on;
-    plot(X_ini, Y_ini, 'o');
+    plot(X_ini(2:3), Y_ini(2:3), 'o', 'Color', [0 0 0]);
+    hold on;
+    plot(X_ini(1), Y_ini(1), 'x', 'Color', [0 0 0]);
     hold on;
     h = animatedline();
     h1 = animatedline();
@@ -35,9 +41,10 @@ function [output] = plot_traj(X_ini, Y_ini, obj_mov, hand_mov, size, mod, fast)
     grid on;
     
     title('Trajectory Dynamic');
-    legend({'Start','Object','Hand'},'FontSize', 6,'Location','northwest')
+    legend({'Start.O.H.', 'Start','Object','Hand'},'FontSize', 6,'Location','northwest')
     legend('boxoff');
-   
+    xlabel(ax1,'Movement(cm.)', 'Color', [0 0.5 0]);
+    ylabel(ax1,'Movement(cm.)', 'Color', [0 0.5 0]);
     
     for t = 1:size+1
         addpoints(h1,hand_mov(1,t),hand_mov(2,t));
