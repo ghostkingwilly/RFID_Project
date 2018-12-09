d4f2b64d10e11e5c0f911b3df1182bdaebbf3faf
Willy <willyhome85@gmail.com>
2018-12-09 10:39:56 +0800

    test the 2pi

--- Code/RNN_Model/tt.py
+++ Code/RNN_Model/tt.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import numpy as np
+import math
 
 a = np.array([[1,2,3,4,5,6,7,8],[2,3,4,5,6,7,8,9],[3,4,5,6,7,8,9,10]])
-b = a.reshape(3,1,8)
+a = a / math.pi
+#b = a.reshape(3,1,8)
 
 #print(b)
 c = pd.DataFrame(a)
 
 d = np.array(c)
 
-print(int(2.54))
+print(a)
 
 
