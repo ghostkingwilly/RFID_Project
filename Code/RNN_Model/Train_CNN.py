eb348a534a5172b9bcebfbe8367a9128a027f35a
Willy <willyhome85@gmail.com>
2018-12-09 10:39:30 +0800

    /2pi plot the value

--- Code/RNN_Model/Train_CNN.py
+++ Code/RNN_Model/Train_CNN.py
@@ -6,6 +6,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D, Conv1D, MaxPooling1D
 import os
+import math
 
 batch_size = 32
 num_classes = 10
@@ -38,6 +39,11 @@ Y_train = np.array(label_df)
 X_arr = np.array(test_trsps)
 Y_test = np.array(Y_Prepare)
 
+# /= 2pi
+two_pi = 2 * math.pi
+train_arr = train_arr / two_pi
+X_arr = X_arr / two_pi
+
 X_train = train_arr.reshape(int(Y_train.shape[0]), 2, int(train_arr.shape[1]))
 X_test = X_arr.reshape(int(Y_test.shape[0]), 2, int(X_arr.shape[1]))
 
@@ -49,7 +55,7 @@ y_train = keras.utils.to_categorical(Y_train)
 y_test = keras.utils.to_categorical(Y_test)
 
 model = Sequential()
-# 32 3*3 kernels 
+# 32 3 kernels 
 model.add(Conv1D(32, 3, padding='same',input_shape=X_train.shape[1:]))
 model.add(Activation('relu'))
 #model.add(Conv1D(32, 3))
@@ -78,7 +84,7 @@ opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
 model.compile(loss='categorical_crossentropy',
               optimizer=opt,
               metrics=['accuracy'])
-model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_test, y_test), shuffle=True)
+history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_test, y_test), shuffle=True)
 """
 # Save model and weights
 if not os.path.isdir(save_dir):
@@ -91,3 +97,33 @@ print('Saved trained model at %s ' % model_path)
 scores = model.evaluate(X_test, y_test, verbose=1)
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
+
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train'], loc='upper right')
+ 
+classes = model.predict(X_test, batch_size=1)
+predicted = np.reshape(classes, (classes.size,))
+Q = np.array([])
+for i in range(0,len(classes)):
+    if classes[i][0]>=0.5:
+        Q = np.hstack((Q,int(1)))
+    else:
+        Q = np.hstack((Q,0))
+#print(Q.astype(int))
+print(classes)
+print (Q)
+plt.subplot(212)
+plt.plot(Y_test)
+plt.plot(Q)
+plt.title('Test Result')
+plt.ylabel('mode')
+plt.xlabel('sample number')
+plt.legend(['Origin', 'Predict'], loc='upper right')
+plt.show()
